6dfcf9f8127e5f4cd9479a9d386f6a6cc21d836d
Collins O. Odhiambo <comon928@gmail.com>
2023-11-14 22:47:13 +0000

    update deployment lint

--- iebcApi/settings.py
+++ iebcApi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vb=coy%b!!k=ne0xg%c#cr+7^$yba^a0yhs+#*e38)p6^fmbpt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://iebc-api.azurewebsites.net', 'http://127.0.0.1:8000']
 
 
 # Application definition
